bbc50d6a0488b2c493b9fc26179316f0a8003646
unknown <willreubena@gmail.com>
2026-06-14 16:32:28 +0100

    Inital commit

---
+++ src/main/java/com/github/willatkinson/invoice_api/InvoiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.willatkinson.invoice_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/willatkinson/invoice_api/InvoiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.willatkinson.invoice_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
